ed43369d7ac6804026d3ce3e6d901602f61d0b19
Elisney <elisneyhenrique.eh@gmail.com>
2025-07-16 12:35:26 -0300

    add sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,13 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
     id("com.google.dagger.hilt.android") version "2.56.2" apply false
+    id("org.sonarqube") version "6.2.0.5505"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "elisney_pepeu")
+        property("sonar.organization", "elisney")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
